f3d2daea138158e24bc7cd8fd3e937c569431156
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-02-12 22:17:20 +0000

    Item8517: missing files in package and svn
    git-svn-id: http://svn.foswiki.org/trunk/ImagePlugin@6307 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/ImagePlugin.pm
+++ lib/Foswiki/Plugins/ImagePlugin.pm
@@ -31,7 +31,7 @@ use vars qw(
 );
 
 $VERSION = '$Rev$';
-$RELEASE = '2.30';
+$RELEASE = '2.31';
 
 use Foswiki::Plugins ();
 use Foswiki::Render ();
--- pub/System/ImagePlugin/jquery.imagetooltip.js
+++ pub/System/ImagePlugin/jquery.imagetooltip.js
@@ -2,4 +2,4 @@ jQuery(function($){var defaults={delay:300,track:true,showURL:false};if(!$.brows
 "topic="+opts.web+"."+opts.topic+";"+
 "file="+opts.image+";"+
 "width="+(opts.width||300)+";"+
-"height="+(opts.height||300);var img=$("<img/>").attr('src',src);return $("<div class='imgTooltip'></div>").append(img);}});}});}});;
+"height="+(opts.height||300);var img=$("<img/>").attr('src',src);return $("<div class='imgTooltip'></div>").append(img);}});}});}});
