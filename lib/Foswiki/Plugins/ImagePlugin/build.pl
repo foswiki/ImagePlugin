09d4d57b530bb277407bedfbb7df574c02d854fb
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-01 14:07:26 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule
    git-svn-id: http://svn.foswiki.org/trunk/ImagePlugin@14680 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/ImagePlugin/build.pl
+++ lib/Foswiki/Plugins/ImagePlugin/build.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
-BEGIN { unshift @INC, split(/:/, $ENV{FOSWIKI_LIBS}); }
+BEGIN { unshift @INC, split( /:/, $ENV{FOSWIKI_LIBS} ); }
 use Foswiki::Contrib::Build;
 
 $build = new Foswiki::Contrib::Build('ImagePlugin');
-$build->build($build->{target});
+$build->build( $build->{target} );
 
