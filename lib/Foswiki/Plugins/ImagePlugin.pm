48d8f6daace12402f64f93dd502e55e957d9e9a8
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/ImagePlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/ImagePlugin.pm
+++ lib/Foswiki/Plugins/ImagePlugin.pm
@@ -25,126 +25,137 @@
 package Foswiki::Plugins::ImagePlugin;
 
 use strict;
-use vars qw( 
+use vars qw(
   $VERSION $RELEASE $imageCore $baseWeb $baseTopic
   $origRenderExternalLink $doneRegisterJQueryPlugin
   $NO_PREFS_IN_TOPIC
 );
 
-$VERSION = '$Rev$';
-$RELEASE = '2.41';
+$VERSION           = '$Rev$';
+$RELEASE           = '2.41';
 $NO_PREFS_IN_TOPIC = 1;
 
 use Foswiki::Plugins ();
-use Foswiki::Render ();
+use Foswiki::Render  ();
 
 ###############################################################################
 sub initPlugin {
-  ($baseTopic, $baseWeb) = @_;
+    ( $baseTopic, $baseWeb ) = @_;
 
-  # check for Plugins.pm versions
-  if( $Foswiki::Plugins::VERSION < 1.026 ) {
-    Foswiki::Func::writeWarning( "Version mismatch between ImagePlugin and Plugins.pm" );
-    return 0;
-  }
+    # check for Plugins.pm versions
+    if ( $Foswiki::Plugins::VERSION < 1.026 ) {
+        Foswiki::Func::writeWarning(
+            "Version mismatch between ImagePlugin and Plugins.pm");
+        return 0;
+    }
 
-  Foswiki::Func::addToZone("head", "IMAGEPLUGIN::META", <<"HERE");
+    Foswiki::Func::addToZone( "head", "IMAGEPLUGIN::META", <<"HERE");
 <meta name='foswiki.ImagePluginEnabled' content='true' />
 HERE
 
-  # init plugin variables
-  $imageCore = undef;
-  $doneRegisterJQueryPlugin = 0;
+    # init plugin variables
+    $imageCore                = undef;
+    $doneRegisterJQueryPlugin = 0;
 
-  # register the tag handlers
-  Foswiki::Func::registerTagHandler( 'IMAGE', sub {
-    getCore($baseWeb, $baseTopic, shift)->handleIMAGE(@_); 
-  });
+    # register the tag handlers
+    Foswiki::Func::registerTagHandler(
+        'IMAGE',
+        sub {
+            getCore( $baseWeb, $baseTopic, shift )->handleIMAGE(@_);
+        }
+    );
 
-  # register rest handler
-  Foswiki::Func::registerRESTHandler('resize', sub {
-    getCore($baseWeb, $baseTopic, shift)->handleREST(@_); 
-  });
+    # register rest handler
+    Foswiki::Func::registerRESTHandler(
+        'resize',
+        sub {
+            getCore( $baseWeb, $baseTopic, shift )->handleREST(@_);
+        }
+    );
 
-
-  # SMELL: monkey-patching Foswiki::Render::_externalLink()
-  if ($Foswiki::cfg{ImagePlugin}{RenderExternalImageLinks}) {
-    unless ($origRenderExternalLink) {
-      no warnings 'redefine';
-      $origRenderExternalLink = \&Foswiki::Render::_externalLink;
-      *Foswiki::Render::_externalLink = \&renderExternalLink;
+    # SMELL: monkey-patching Foswiki::Render::_externalLink()
+    if ( $Foswiki::cfg{ImagePlugin}{RenderExternalImageLinks} ) {
+        unless ($origRenderExternalLink) {
+            no warnings 'redefine';
+            $origRenderExternalLink         = \&Foswiki::Render::_externalLink;
+            *Foswiki::Render::_externalLink = \&renderExternalLink;
+        }
     }
-  }
 
-  # register jquery.imagetooltip plugin if jquery is isntalled
-  if ($Foswiki::cfg{Plugins}{JQueryPlugin}{Enabled}) {
-    require Foswiki::Plugins::JQueryPlugin;
-    Foswiki::Plugins::JQueryPlugin::registerPlugin("ImageTooltip",
-      'Foswiki::Plugins::ImagePlugin::IMAGETOOLTIP');
-  }
+    # register jquery.imagetooltip plugin if jquery is isntalled
+    if ( $Foswiki::cfg{Plugins}{JQueryPlugin}{Enabled} ) {
+        require Foswiki::Plugins::JQueryPlugin;
+        Foswiki::Plugins::JQueryPlugin::registerPlugin( "ImageTooltip",
+            'Foswiki::Plugins::ImagePlugin::IMAGETOOLTIP' );
+    }
 
-  # Plugin correctly initialized
-  return 1;
-} 
+    # Plugin correctly initialized
+    return 1;
+}
 
 ###############################################################################
 # lazy initializer
 sub getCore {
-  return $imageCore if $imageCore;
+    return $imageCore if $imageCore;
 
-  Foswiki::Func::addToZone("head", "IMAGEPLUGIN", <<'HERE');
+    Foswiki::Func::addToZone( "head", "IMAGEPLUGIN", <<'HERE');
 <link rel="stylesheet" href="%PUBURL%/%SYSTEMWEB%/ImagePlugin/style.css" type="text/css" media="all" />
 HERE
 
-  require Foswiki::Plugins::ImagePlugin::Core;
-  $imageCore = new Foswiki::Plugins::ImagePlugin::Core(@_);
-  return $imageCore;
+    require Foswiki::Plugins::ImagePlugin::Core;
+    $imageCore = new Foswiki::Plugins::ImagePlugin::Core(@_);
+    return $imageCore;
 }
 
 ###############################################################################
 sub renderExternalLink {
-  my ($this, $url, $text) = @_;
+    my ( $this, $url, $text ) = @_;
 
-  my $href = '';
-  my $title = $url;
-  $text ||= '';
+    my $href  = '';
+    my $title = $url;
+    $text ||= '';
 
-  #print STDERR "called renderExternalLink($url, $text)\n";
+    #print STDERR "called renderExternalLink($url, $text)\n";
 
-  # also render an image tag for links that have an external image link as a text
-  if ($text =~ /^(https?:).*\.(gif|jpg|jpeg|png)$/i) {
-    $href = $url;
-    $title = $url;
-    $url = $text;
-    $text = '';
-  }
-
-  if ($url =~ /^(https?:).*\.(gif|jpg|jpeg|png)/i && !$text) {
-    my $pubUrl = $this->{session}->getPubUrl(1);
-    # skip "external links" to self and to any other excluded url
-    my $excludePattern = $Foswiki::cfg{ImagePlugin}{Exclude};
-    if ($url !~ /^$pubUrl/ && 
-        (!$excludePattern || $url !~ /^$excludePattern()/)) { 
-
-      # untaint url, check above
-      $url = Foswiki::Sandbox::untaintUnchecked($url);
-      $url =~ s/\?.*$//;
-
-      my $params = {
-	_DEFAULT => "$url",
-	href => $href,
-	title => $title,
-	type => 'simple',
-      };
-
-      return getCore($baseWeb, $baseTopic)->handleIMAGE($params, $baseTopic, $baseWeb);
-    } else {
-      #print STDERR "normal handling of $url\n";
+ # also render an image tag for links that have an external image link as a text
+    if ( $text =~ /^(https?:).*\.(gif|jpg|jpeg|png)$/i ) {
+        $href  = $url;
+        $title = $url;
+        $url   = $text;
+        $text  = '';
     }
-  }
 
-  # normal
-  return &{$origRenderExternalLink}($this, $url, $text);
+    if ( $url =~ /^(https?:).*\.(gif|jpg|jpeg|png)/i && !$text ) {
+        my $pubUrl = $this->{session}->getPubUrl(1);
+
+        # skip "external links" to self and to any other excluded url
+        my $excludePattern = $Foswiki::cfg{ImagePlugin}{Exclude};
+        if ( $url !~ /^$pubUrl/
+            && ( !$excludePattern || $url !~ /^$excludePattern()/ ) )
+        {
+
+            # untaint url, check above
+            $url = Foswiki::Sandbox::untaintUnchecked($url);
+            $url =~ s/\?.*$//;
+
+            my $params = {
+                _DEFAULT => "$url",
+                href     => $href,
+                title    => $title,
+                type     => 'simple',
+            };
+
+            return getCore( $baseWeb, $baseTopic )
+              ->handleIMAGE( $params, $baseTopic, $baseWeb );
+        }
+        else {
+
+            #print STDERR "normal handling of $url\n";
+        }
+    }
+
+    # normal
+    return &{$origRenderExternalLink}( $this, $url, $text );
 }
 
 1;
